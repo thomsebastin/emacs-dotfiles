fb9f31a1d10fd5119363cf6f2ae6e0b09efbdcc6
Thomas Sebastian <thomsebastin@gmail.com>
2017-08-26 20:05:02 +0530

    packages required

---
+++ elpa/font-lock+-20170222.1755/font-lock+-autoloads.el
@@ -0,0 +1,15 @@
+;;; font-lock+-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("font-lock+.el") (22945 29434 612787 585000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; font-lock+-autoloads.el ends here
---
+++ elpa/gruvbox-theme-20170821.1241/gruvbox-theme-autoloads.el
@@ -0,0 +1,80 @@
+;;; gruvbox-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "gruvbox" "gruvbox.el" (22945 30167 599594
+;;;;;;  878000))
+;;; Generated autoloads from gruvbox.el
+
+(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil "gruvbox-dark-hard-theme" "gruvbox-dark-hard-theme.el"
+;;;;;;  (22945 30167 799604 703000))
+;;; Generated autoloads from gruvbox-dark-hard-theme.el
+
+(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil "gruvbox-dark-medium-theme" "gruvbox-dark-medium-theme.el"
+;;;;;;  (22945 30167 767603 131000))
+;;; Generated autoloads from gruvbox-dark-medium-theme.el
+
+(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil "gruvbox-dark-soft-theme" "gruvbox-dark-soft-theme.el"
+;;;;;;  (22945 30167 835606 472000))
+;;; Generated autoloads from gruvbox-dark-soft-theme.el
+
+(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil "gruvbox-light-hard-theme" "gruvbox-light-hard-theme.el"
+;;;;;;  (22945 30167 631596 450000))
+;;; Generated autoloads from gruvbox-light-hard-theme.el
+
+(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil "gruvbox-light-medium-theme" "gruvbox-light-medium-theme.el"
+;;;;;;  (22945 30167 867608 43000))
+;;; Generated autoloads from gruvbox-light-medium-theme.el
+
+(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil "gruvbox-light-soft-theme" "gruvbox-light-soft-theme.el"
+;;;;;;  (22945 30167 667598 219000))
+;;; Generated autoloads from gruvbox-light-soft-theme.el
+
+(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil "gruvbox-theme" "gruvbox-theme.el" (22945 30167
+;;;;;;  567593 306000))
+;;; Generated autoloads from gruvbox-theme.el
+
+(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil nil ("gruvbox-theme-pkg.el") (22945 30167 937370
+;;;;;;  441000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; gruvbox-theme-autoloads.el ends here
